70981f455797f751c268a3f166dfcf76c333f642
jeahun10717 <jeahun10717@gmail.com>
2020-11-13 14:01:11 +0900

    view instance delete

--- expressStudyByFastCampus/additionalSouceCode/4_view상속/producerRoutes/producer.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const routerOfProducer = express.Router();
-
-routerOfProducer.get('/', (req, res)=>{
-    res.send('URL : localhost:3300/producer');
-});
-
-module.exports = routerOfProducer;
